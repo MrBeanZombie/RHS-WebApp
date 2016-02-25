848ae8bcbc48339798daa23f75dd5c3a77b74401
System Administrator <root@F103-PrintServer.local>
2016-02-25 07:59:55 -1000

    Commit

--- parents.js
+++ parents.js
@@ -2,5 +2,7 @@ function forParents() {
   document.getElementById('showParentsNews').innerHTML =
   '<br>' +
   '<p> This peice of text is being pulled from a JavaScript file called parents.js on the howardkam.com server. </p>' +
-  '<p> The intention of this text is for the WebDev team to input general notices from the Admin that parents should know.</p>';
+  '<p> The intention of this text is for the WebDev team to input general notices from the Admin that parents should know. This JS file will have to be built every time. You can go to howardkam.com/parents.js to view the code. </p>' 
+
+  ;
 }

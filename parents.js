15f40a3656abf80bb486ad1a92b5c0214da58ba9
Howard Kam <hkam@roosevelths.k12.hi.us>
2016-02-25 07:04:36 -1000

    Added parents section

---
+++ parents.js
@@ -0,0 +1,6 @@
+function forParents() {
+  document.getElementById('showParentsNews').innerHTML =
+  '<br>' +
+  '<p> This peice of text is being pulled from a JavaScript file called parents.js on the howardkam.com server. </p>' +
+  '<p> The intention of this text is for the WebDev team to input general notices from the Admin that parents should know.</p>';
+}
